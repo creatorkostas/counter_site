10d498250d214a101190a7e0ea66c444fb0cb3e4
creatorkostas <iee2021106@iee.ihu.gr>
2022-04-11 20:28:52 +0300

    Put comments

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+//Increase the number from the paragraph element
 function increase(){
-    counterObj = document.getElementsByClassName("counter")[0];
+    counterObj = document.getElementsByClassName("counter")[0]; //Get the element
     counterVal = counterObj.innerHTML;
     counterObj.innerHTML = parseInt(counterVal) + 1;
 }
 
+//Decrease the number from the paragraph element
 function decrease(){
     counterObj = document.getElementsByClassName("counter")[0];
     counterVal = counterObj.innerHTML;
